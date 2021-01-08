cfc5c49d23da856d44f629e9e5a5f601aca1dd6c
Stenio <steniooliv@gmail.com>
2021-01-07 23:04:45 -0300

    create home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 import './style.css';
 
+import Linkedin from '../../assets/linkedin.svg';
+import Github from '../../assets/github.svg';
+
 export default function Home() {
+
   return (
     <div className="wrapper">
-      <p>steniooliv</p>
+      <div className="modal">
+        <div className="logo">
+        </div>
+        <div className="social">
+          <p>steniooliv</p>
+          <div className="buttons">
+            <a href="https://www.linkedin.com/in/steniooliv/">
+              <img src={Linkedin} alt="Linkedin"/>
+            </a>
+            <a href="https://www.github.com/steniooliv">
+              <img src={Github} alt="Github"/>
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
