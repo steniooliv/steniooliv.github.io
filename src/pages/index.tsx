edd107aebd6cb6f738d704b33f070c49bd98bf7c
Stenio Oliveira <steniooliv@gmail.com>
2021-10-21 15:43:48 -0300

    update flexbox

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,48 +1,48 @@
-import Head from 'next/head';
-
 import styles from '../styles/home.module.scss';
 
-export default function Home() {
+export default function Home(){
   return (
     <>
-    <Head>
-      <title>Stênio Oliveira</title>
-    </Head>
-    
-    <main className={styles.container}>
-      <header className={styles.header}>
+      <div className={styles.wrapper}>
+
+        <header className={styles.header}>
           <img src="/assets/steniooliv_profile.png" alt="Stenio Oliveira" />
-      </header>
-      <article className={styles.about}>
-        <h1>Stenio Oliveira</h1>
-        <p>IT manager for fifteen years, web developer with knowledge in React and Next.js technologies, mobile developer with insights in Flutter, I live in Brazil working at Raro Labs as a Flutter Developer.</p>
-        <div className={styles.contact}>
-          <h1>Contact me at</h1>
-          <div>
+        </header>
+
+        <main className={styles.content}>
+
+          <h1>Stenio Oliveira</h1>
+          <p>IT manager for fifteen years, web developer with knowledge in React and Next.js technologies, mobile developer with insights in Flutter, I live in Brazil working at Raro Labs as a Flutter Developer.</p>
+          
+          <div className={styles.contact}>
+            <h1>Contact me at</h1>
             <div>
-              <a href="https://www.linkedin.com/in/steniooliv">
-                <img src="/assets/linkedin.svg" alt="Linkedin" />
-              </a>
-              <a href="https://www.linkedin.com/in/steniooliv">
-                <span>/in/steniooliv</span>
-              </a>
-            </div>
-            <div>
-              <a href="https://www.github.com/steniooliv">
-                <img src="/assets/github.svg" alt="Github" />
-              </a>
-              <a href="https://www.github.com/steniooliv">
-                <span>/steniooliv</span>
-              </a>
+              <div>
+                <a href="https://www.linkedin.com/in/steniooliv">
+                  <img src="/assets/linkedin.svg" alt="Linkedin" />
+                </a>
+                <a href="https://www.linkedin.com/in/steniooliv">
+                  <span>/in/steniooliv</span>
+                </a>
+              </div>
+              <div>
+                <a href="https://www.github.com/steniooliv">
+                  <img src="/assets/github.svg" alt="Github" />
+                </a>
+                <a href="https://www.github.com/steniooliv">
+                  <span>/steniooliv</span>
+                </a>
+              </div>
             </div>
           </div>
-        </div>
-      </article>
 
-      <footer className={styles.footer}>
-        <img src="/assets/steniooliv.svg" alt="steniooliv" />
-      </footer>
-    </main>
+        </main>
+
+        <footer className={styles.footer}>
+          <img src="/assets/steniooliv.svg" alt="steniooliv" />
+        </footer>
+        
+      </div>
     </>
   );
 }
